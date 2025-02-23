3f56c5c4c8a32b061cad7ed9eff032840202a03d
Santrulu <santrululu12@gmail.com>
2025-02-23 14:37:36 -0500

    Se solucionó el problema de la fuente Ink Free

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import './styles/fonts.css';
+import App from "./App";
+import "./index.css";
+import "./styles/fonts.css"; // Ruta corregida
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
