51887701e584cb49451046081d80f3b0fcbace25
Santrulu <santrululu12@gmail.com>
2025-04-04 09:03:07 -0500

    Corregido script de build para Windows

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from "./App";
 import "./index.css";
 import "./styles/fonts.css"; // Ruta corregida
@@ -7,5 +7,5 @@ import "./styles/fonts.css"; // Ruta corregida
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
